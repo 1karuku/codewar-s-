53928cd592f1fec683f797599f40bb51dddcee24
1karuku <robert.n.karuku@gmail.com>
2022-05-13 20:09:20 +0300

    how to return a string

---
+++ js/solution001.js
@@ -0,0 +1,7 @@
+/*Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".*/
+
+//create a greet function
+function greet(name){
+    //create return message
+    return (`Hello ${name} how are you doing today?`)
+}
