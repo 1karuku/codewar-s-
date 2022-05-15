2e3bafff0ebce4e82fb8b4e4dc3d414dad9f1777
1karuku <robert.n.karuku@gmail.com>
2022-05-15 13:07:56 -0300

    a better solution

--- js/solution002.js
+++ js/solution002.js
@@ -1,6 +1,6 @@
 //Rock Paper Scissors
 /*Let's play! You have to return which player won! In case of a draw return Draw!.*/
-const rps = (p1, p2) => {
+/*const rps = (p1, p2) => {
     const rps = (p1, p2) => {
         if ((p1 === 'paper' && p2 === 'rock') || (p1 === 'rock' && p2 ==='scissors') || (p1 ==='scissors' && p2 === 'paper')){
             return 'Player 1 won!'
@@ -11,4 +11,17 @@ const rps = (p1, p2) => {
         }
         };
   }
-;
+;*/
+//above is my  sollution
+
+//this is how it should be done
+const rps = (p1, p2) => {
+  if (p1 === p2) return "Draw!";
+  var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+  if (p2 === rules[p1]) {
+    return "Player 1 won!";
+  }
+  else {
+    return "Player 2 won!";
+  }
+};
